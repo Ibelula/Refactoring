3d559acc9e6b931409a7ef03ec4b010b9c445cad
Rodolfo M. Marcelino <anhell27@hotmail.com>
2021-11-21 23:04:40 +0100

    Add test and Min

---
+++ IntervalUnitTets/models/Min.cs
@@ -0,0 +1,14 @@
+﻿public class Min
+{
+	protected double value;
+
+	public Min(double value)
+	{
+		this.value = value;
+	}
+
+	public bool isWithin(double value)
+	{
+		return this.value < value;
+	}
+}
---
+++ IntervalUnitTets/tests/MinTest.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+using FluentAssertions;
+using Moq;
+public class MinTest
+{
+
+    protected Min min;
+
+    //@BeforeEach
+    public void before()
+    {
+        this.min = new Min(NumberLine.VALUE);
+    }
+
+    [Test]
+    public void givenMinWhenIsWithinWithLessValueThenTrue()
+    {
+        Assert.False(this.min.isWithin(NumberLine.less(NumberLine.VALUE)));
+    }
+
+    [Test]
+    public void givenMinWhenIsWithinWithEqualsValue()
+    {
+        Assert.False(this.min.isWithin(NumberLine.equals(NumberLine.VALUE)));
+    }
+
+    [Test]
+    public void givenMinWhenIsWithinWithGreaterValueThenTrue()
+    {
+        Assert.True(this.min.isWithin(NumberLine.greater(NumberLine.VALUE)));
+    }
+}
