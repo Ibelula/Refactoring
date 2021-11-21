9b621ca962f6e9955ea37ac0ecca9d9bab953017
Rodolfo M. Marcelino <anhell27@hotmail.com>
2021-11-21 21:45:12 +0100

    Green fix error interval of

--- IntervalUnitTets/models/Interval.cs
+++ IntervalUnitTets/models/Interval.cs
@@ -2,16 +2,20 @@
 {
 	private double min;
 	private double max;
+	private bool maxOpen;
 
 	public Interval(bool minOpen, double min, bool maxOpen, double max)
 	{
 		Test.Assert(min <= max);
 		this.min = min;
 		this.max = max;
+		this.maxOpen = maxOpen;
 	}
 
 	public bool include(double value)
 	{
+		if (this.maxOpen)
+			return this.min <= value && value < this.max;
 		return this.min <= value && value <= this.max;		
 	}
 }
