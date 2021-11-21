a82933286a063360c98dedb0e845b2e5b4a7806c
Rodolfo M. Marcelino <anhell27@hotmail.com>
2021-11-21 21:48:08 +0100

    Refactor, Red, greater on Max

---
+++ IntervalUnitTets/models/Max.cs
@@ -0,0 +1,12 @@
+﻿
+public class Max
+{
+	public Max(bool b, int i)
+	{
+	}
+
+	public bool greater(double d)
+	{
+		return false;
+	}
+}
---
+++ IntervalUnitTets/tets/Class1.cs
@@ -0,0 +1,11 @@
+﻿using Xunit;
+using FluentAssertions;
+using Moq;
+public class MaxTest
+{
+    [Test]
+    public void givenIntervalwhenIncludeWithIncludedValueThenTrue()
+    {
+        Assert.True(new Max(true, 4).greater(0.0));
+    }
+}
