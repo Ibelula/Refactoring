eaada0ae533b22b5b0083818ef53366fb79067fe
Rodolfo M. Marcelino <anhell27@hotmail.com>
2021-11-21 21:56:27 +0100

    rectator, green

--- IntervalUnitTets/models/Max.cs
+++ IntervalUnitTets/models/Max.cs
@@ -1,12 +1,19 @@
 ﻿
 public class Max
 {
+	private double value;
+	private bool open;
+
 	public Max(bool b, int i)
 	{
+		this.value = i;
+		this.open = b;
 	}
 
 	public bool greater(double d)
 	{
-		return true;
+		if (this.open)
+			return this.value > d;
+		return this.value >= d;
 	}
 }
